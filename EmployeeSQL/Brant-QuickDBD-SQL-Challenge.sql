00caff6bb6328050d39c5e8a1954e0a2cc72aad4
Brant <brant@Apples-Mac.local>
2019-09-21 13:41:17 -0500

    Updated Homework Files

--- EmployeeSQL/Brant-QuickDBD-SQL-Challenge.sql
+++ EmployeeSQL/Brant-QuickDBD-SQL-Challenge.sql
@@ -5,11 +5,11 @@
 
 CREATE TABLE "employees" (
     "emp_no" int   NOT NULL,
-    "birth_date" datetime   NOT NULL,
-    "first_name" string   NOT NULL,
-    "last_name" string   NOT NULL,
-    "gender" string   NOT NULL,
-    "hire_date" datetime   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar   NOT NULL,
+    "last_name" varchar   NOT NULL,
+    "gender" varchar   NOT NULL,
+    "hire_date" date   NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
      )
@@ -18,37 +18,37 @@ CREATE TABLE "employees" (
 CREATE TABLE "salaries" (
     "emp_no" int   NOT NULL,
     "salary" float   NOT NULL,
-    "from_date" datetime   NOT NULL,
-    "to_date" datetime   NOT NULL
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
 );
 
 CREATE TABLE "dept_emp" (
     "emp_no" int   NOT NULL,
-    "dept_no" string   NOT NULL,
-    "from_date" datatime   NOT NULL,
-    "to_date" datetime   NOT NULL
+    "dept_no" varchar   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
 );
 
 CREATE TABLE "departments" (
-    "dept_no" string   NOT NULL,
-    "dept_name" string   NOT NULL,
+    "dept_no" varchar   NOT NULL,
+    "dept_name" varchar   NOT NULL,
     CONSTRAINT "pk_departments" PRIMARY KEY (
         "dept_no"
      )
 );
 
 CREATE TABLE "dept_manager" (
-    "dept_no" string   NOT NULL,
+    "dept_no" varchar   NOT NULL,
     "emp_no" int   NOT NULL,
-    "from_date" datetime   NOT NULL,
-    "to_date" datetime   NOT NULL
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
 );
 
 CREATE TABLE "titles" (
     "emp_no" int   NOT NULL,
-    "title" string   NOT NULL,
-    "from_date" datetime   NOT NULL,
-    "to_date" datetime   NOT NULL
+    "title" varchar   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
 );
 
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
